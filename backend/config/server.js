f2feca3e01e7e86763b93dd0bd8a2b5b638dc72c
knyeol <knyeol@outlook.com>
2021-01-07 11:22:59 -0800

    updated backend

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'aefa3bab1af2e9e68abbdada3b1daa8d'),
-    },
-  },
+      secret: env("ADMIN_JWT_SECRET", "aefa3bab1af2e9e68abbdada3b1daa8d")
+    }
+  }
 });
