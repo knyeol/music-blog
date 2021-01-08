90b668b7d3351e72eb32e7311875b1a501090c37
knyeol <knyeol@outlook.com>
2021-01-07 19:26:08 -0800

    updated backend

--- frontend/src/containers/Header.js
+++ frontend/src/containers/Header.js
@@ -1,11 +1,9 @@
 function Header() {
-  const url = "/uploads/wengang_zhai_c_R_Em_Xf_Mvs_Z0_unsplash_20aaa1f897.jpg";
+  const url =
+    "https://res.cloudinary.com/knyeol/image/upload/v1609973570/wengang_zhai_c_R_Em_Xf_Mvs_Z0_unsplash_20aaa1f897.jpg";
 
   return (
-    <div
-      className="header"
-      style={{ backgroundImage: `url(${process.env.REACT_APP_URL + url})` }}
-    >
+    <div className="header" style={{ backgroundImage: `url(${url})` }}>
       <div className="header-overlay">
         <h2>Piano, Music Theory, and Composition</h2>
       </div>
